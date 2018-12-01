8cd6d263357264f8142d18916fa90fe12299924a
Smeetss <44531825+Smeetss@users.noreply.github.com>
2018-12-01 11:51:18 -0800

    Add files via upload

--- FlightPlots/FlightPlots.m
+++ FlightPlots/FlightPlots.m
@@ -82,27 +82,48 @@ tics_per_sec = 13;
 time = 1:tics_per_sec:high-low;
 range = low:tics_per_sec:high;
 
-% 1st Launch Temp
+low2 = 42605;
+high2 = 42685;
+tics_per_sec2 = 13;
+time2 = 1:tics_per_sec2:high2-low2;
+range2 = low2:tics_per_sec2:high2;
+
+% Launch Temp
 figure
 plot(1:length(time),TEMP(range))
+hold on
+plot(1:length(time2),TEMP(range2))
+plot(length(time2), TEMP(range2(end)), 'rx')
 xlim([0 100])
-ylim([40 60])
+ylim([30 60])
+legend('Run 1', 'Run 2', 'Detached from rocket')
 title('Temperature')
 xlabel('Time [s]')
 ylabel('Temperature [C]')
 
+% Launch Pressure
 figure
 plot(1:length(time),PRESSURE(range))
+hold on
+plot(1:length(time2),PRESSURE(range2))
+plot(length(time2), PRESSURE(range2(end)), 'rx')
 xlim([0 100])
-ylim([88000 94700])
+ylim([87000 94700])
+legend('Run 1', 'Run 2', 'Detached from rocket')
 title('Pressure')
 xlabel('Time [s]')
 ylabel('Pressure [Pa]')
 
+% Launch Altitude
+
 figure
 plot(1:length(time),ALTITUDE(range) - 593)
+hold on
+plot(1:length(time2),ALTITUDE(range2) - 597)
+plot(length(time2), ALTITUDE(range2(end)) - 597, 'rx')
 xlim([0 100])
-ylim([0 600])
+ylim([0 700])
+legend('Run 1', 'Run 2', 'Detached from rocket')
 title('Rocket Altitude')
 xlabel('Time [s]')
 ylabel('Altitude [m]')
